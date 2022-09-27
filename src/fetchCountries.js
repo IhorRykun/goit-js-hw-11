bf757158dd6a4419ff16fe29ebe69a5b57059e88
Ihor <ihorrykun@gmail.com>
2022-09-27 20:11:07 +0300

    оновили функцію запиту

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,11 @@
 //  !   функція запиту API
 
-const url = `https://pixabay.com/api/?`;
- const keyApi = '30187143-4d7f5699d03729238b163605a';
-function fetchCountries(name) {
-  return fetch(`${url}${name}/${keyApi}`).then(response => {
+const url = `https://pixabay.com/api`;
+ const key = '30187143-4d7f5699d03729238b163605a';
+function fetchCountries() {
+  return fetch(
+    `${url}?key=${key}&image_type=photo&orientation=horizontal`
+  ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
--- src/index.js
+++ src/index.js
@@ -5,3 +5,19 @@ import Notiflix from 'notiflix';
 
 //  ! Експорт  функції  запиту API
 import { fetchCountries } from './fetchCountries';
+
+
+const form = document.querySelector('#search-form');
+
+form.addEventListener('submit', submitImg);
+
+
+function submitImg(e) {
+    e.preventDefault();
+    const inputEl = e.currentTarget.searchQuery.value;
+    console.dir(inputEl);
+    fetchCountries();
+
+}
+
+
