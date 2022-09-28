c9c7756323b951df6453bc85673ae1aeba1c0870
Ihor <ihorrykun@gmail.com>
2022-09-28 20:34:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import NewsApiGalleryService from "./fetchImg";
 //  ! Імпорт  функції рендеру розмітки
 import { renderGalery } from './renderGalery';
 
+const div = document.querySelector('.gallery');
 
 let isShow = 0;
 const form = document.querySelector('#search-form');
@@ -18,8 +19,18 @@ let pageNumber = 1;
 form.addEventListener('submit', submitImgForm);
 
 function submitImgForm(e) {
-  e.preventDefault();
-  const inputEl = e.currentTarget.searchQuery.value;
-  pageNumber += 1;
-  renderGalery();
+    e.preventDefault();
+    if (e.currentTarget.elements.searchQuery.value === '') {
+       return innerHTML = '';
+    }
+    GalleryEl.query = e.target.elements.searchQuery.value.trim();
+let isShow = 0;
+    div.innerHTML = '';
+    GalleryEl.resetPage();
+    fetchImg();
+}
+
+function onLoadMore() {
+  GalleryEl.incrementPage();
+  fetchImg();
 }
