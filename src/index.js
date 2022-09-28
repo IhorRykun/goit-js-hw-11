3a622f015bdb72cc5ed54e22ca9a5fcfc4a011b5
Ihor <ihorrykun@gmail.com>
2022-09-28 20:45:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 
 //  ! імпорт  класу  запиту API
-import NewsApiGalleryService from "./fetchImg";
+import NewsApiGalleryService from './fetchImg';
 //  ! Імпорт  функції рендеру розмітки
 import { renderGalery } from './renderGalery';
 
@@ -10,27 +10,40 @@ const div = document.querySelector('.gallery');
 
 let isShow = 0;
 const form = document.querySelector('#search-form');
-// !    Змінна для того щоб отримати об"єкт 
+// !    Змінна для того щоб отримати об"єкт
 const GalleryEl = new NewsApiGalleryService();
-
 
 let pageNumber = 1;
 
 form.addEventListener('submit', submitImgForm);
 
 function submitImgForm(e) {
-    e.preventDefault();
-    if (e.currentTarget.elements.searchQuery.value === '') {
-       return innerHTML = '';
-    }
-    GalleryEl.query = e.target.elements.searchQuery.value.trim();
-let isShow = 0;
-    div.innerHTML = '';
-    GalleryEl.resetPage();
-    fetchImg();
+  e.preventDefault();
+  if (e.currentTarget.elements.searchQuery.value === '') {
+    return (innerHTML = '');
+  }
+  GalleryEl.query = e.target.elements.searchQuery.value.trim();
+  let isShow = 0;
+  div.innerHTML = '';
+  GalleryEl.resetPage();
+  fetchImg();
 }
 
 function onLoadMore() {
   GalleryEl.incrementPage();
   fetchImg();
+}
+
+async function fetchImg() {
+  const response = await GalleryEl.fetchImg();
+  const { hits, total } = response;
+
+  if (!hits.length) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+  renderGalery(hits);
+
+  isShow += hits.length;
 }
